d68f0a443f821a23d968931a789874fe8cee8a4e
NAMLEHOANG\lehoa <lehoangnam.31123@gmail.com>
2026-03-17 00:15:36 +0700

    feat(element-alert): practice alert action with selenium

---
+++ src/aummation_test/AlertElementTest.java
@@ -0,0 +1,46 @@
+package aummation_test;
+
+import java.time.Duration;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AlertElementTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+		driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+
+//		1. click btn
+//		driver.findElement(By.xpath("//button[text()='Click for JS Alert']")).click();
+		driver.findElement(By.xpath("//button[text()='Click for JS Prompt']")).click();
+		
+//		2. chuyển hướng sang Alert
+		Alert promptAlert = driver.switchTo().alert();
+
+//		3. action of alert
+////		3.1 Click ok btn of alert
+//		driver.switchTo().alert().accept();
+////		3.2 Click cancel/Huy btn of alert
+//		driver.switchTo().alert().dismiss();
+////		3.3 Nhập nội dung vào alert
+//		driver.switchTo().alert().sendKeys("Nội dung cần nhập vào prompt alert");
+		
+		promptAlert.sendKeys("Ted - Software Testing Engineer");
+		promptAlert.accept();
+		
+		String result = driver.findElement(By.id("result")).getText();
+		System.out.println("Result: " + result);
+		
+
+//		driver.quit();
+
+	}
+
+}
